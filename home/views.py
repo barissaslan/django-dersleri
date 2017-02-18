3554deeab28de872db5938a6b1c6a01124f12692
Barış Aslan <aslannbaris@gmail.com>
2017-02-18 17:32:22 +0300

    12 - Template Dinamik İçerik

--- home/views.py
+++ home/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 
 def home_view(request):
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated():
+        context = {
+            'isim': 'Barış'
+        }
+    else:
+        context = {
+            'isim': 'Misafir Kullanıcı'
+        }
+    return render(request, 'home.html', context)
