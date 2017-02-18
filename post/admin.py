ee95f2bbcac9bf6e7574ebe0826e6d08ff4f22a0
Barış Aslan <aslannbaris@gmail.com>
2017-02-18 17:17:06 +0300

    8 - Admin Panelini Özelleştirmek

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Post  # veya from post.models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publishing_date']
+    list_display_links = ['publishing_date']
+    list_filter = ['publishing_date']
+    search_fields = ['title', 'content']
+    list_editable = ['title']
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostAdmin)
 
