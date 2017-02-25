013337c460c3154749f2519a2201e278ba81e3fd
Barış Aslan <aslannbaris@gmail.com>
2017-02-26 02:25:02 +0300

    21 - The Messages Framework

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect, redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib import messages
 
 
 def post_index(request):
@@ -36,6 +37,7 @@ def post_create(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
         post = form.save()
+        messages.success(request, "Başarılı bir şekilde oluşturdunuz.", extra_tags='mesaj-basarili')
         return HttpResponseRedirect(post.get_absolute_url())
 
     context = {
@@ -50,6 +52,7 @@ def post_update(request, id):
     form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
         form.save()
+        messages.success(request, "Başarılı bir şekilde güncellediniz.")
         return HttpResponseRedirect(post.get_absolute_url())
 
     context = {
