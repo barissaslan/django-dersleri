6daf0342ccd0b0d3b815093e2fb616f495a132dd
Barış Aslan <aslannbaris@gmail.com>
2017-03-04 03:41:43 +0300

    35 - Django Pagination (Sayfalama)

--- post/views.py
+++ post/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render, HttpResponse, get_object_or_404, HttpRespon
 from .models import Post
 from .forms import PostForm, CommentForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_index(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 5)  # Show 5 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, "post/index.html", {'posts': posts})
 
 
